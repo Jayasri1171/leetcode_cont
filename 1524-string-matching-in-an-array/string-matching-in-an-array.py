c677c2cf694eb3a222ecfc2ac55482243d9c051d
Jaya Sri Durga Perumalla <22A91A0547@aec.edu.in>
2025-01-07 07:44:09 +0530

    Time: 3 ms (64.01%) | Memory: 17.8 MB (19.52%) - LeetSync

---
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        ans=[]
+        for i in range(len(words)):
+            for j in range(len(words)):
+                if i!=j:
+                    if words[i] in words[j]:
+                        ans.append(words[i])
+        ans=list(set(ans))
+        return ans
+
+        
