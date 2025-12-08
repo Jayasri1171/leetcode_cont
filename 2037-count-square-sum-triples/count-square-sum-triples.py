0b805cb7207bcd10a01209a1fd634ab19f80c7e2
Jaya Sri Durga Perumalla <22A91A0547@aec.edu.in>
2025-12-08 21:11:07 +0530

    Time: 183 ms (52.17%) | Memory: 17.7 MB (54.08%) - LeetSync

---
+++ 2037-count-square-sum-triples/count-square-sum-triples.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countTriples(self, n: int) -> int:
+        ans=0
+        for i in range(1,n):
+            for j in range(i+1,n+1):
+                if(sqrt(i*i+j*j)<=n):
+                    z=sqrt(i*i+j*j)
+                    m=i*i+j*j
+                    if(int(z)==z):
+                        # print(i,j,z,m)
+                        ans+=1
+        return ans*2
+
+        
