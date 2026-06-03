9be4ee0c15b58afb03e83bdd134c4ccd2dc910fe
Jaya Sri Durga Perumalla <22A91A0547@aec.edu.in>
2026-06-03 12:31:39 +0530

    Time: 130 ms (40.00%) | Memory: 35.1 MB (38.18%) - LeetSync

---
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
+        firstland=123456789
+        for i in range(len(landStartTime)):
+            z=landStartTime[i]+landDuration[i]
+            # print(z)
+            firstland=min(firstland,z)
+            # print(firstland)
+        secondwater=123456789
+        for i in range(len(waterStartTime)):
+            if(waterStartTime[i]<=firstland):
+                z=waterDuration[i]
+            else:
+                z=waterStartTime[i]+waterDuration[i]-firstland
+            secondwater=min(secondwater,z)
+        firstans=firstland+secondwater
+        firstwater=123456789
+        for i in range(len(waterDuration)):
+            z=waterStartTime[i]+waterDuration[i]
+            firstwater=min(firstwater,z)
+        secondland=123456789
+        for i in range(len(landDuration)):
+            if(landStartTime[i]<=firstwater):
+                z=landDuration[i]
+            else:
+                z=landStartTime[i]+landDuration[i]-firstwater
+            secondland=min(secondland,z)
+        secondans=firstwater+secondland
+        ans=min(firstans,secondans)
+        # print(firstans,secondans,firstland,secondwater,firstwater,secondland)
+        return ans
